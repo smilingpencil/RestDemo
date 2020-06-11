c6fbef1ada6d27263c03c56d9fa06aeb81380724
nareshkalakada <nk00115270@techmahindra.com>
2020-06-11 12:19:21 +0530

    Added devices

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/devices', function(req, res, next) {
+  res.send(devices);
+});
+
+var devices = [
+  {name:'Crusing Machine', sensors: [
+    {name:'temp-sensor', value:100},
+    {name:'humid-sensor', value:10},
+  ]},
+  {name:'Rotary machine', sensors: [
+    {name:'Rotation-speed', value:1000},
+    {name:'count', value:599},
+  ]}
+]
+
 module.exports = router;
